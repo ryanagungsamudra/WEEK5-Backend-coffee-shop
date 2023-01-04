5b103cf24b52b1c50493b2f3b06ae539721109e6
Ryan Agung Samudra <ryansamudra67@gmail.com>
2023-01-04 08:17:00 +0700

    update productDetail endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express();
 const productRoute = require('./product.route')
 const usersRoute = require('./users.route')
+const productDetailRoute = require('./product-detail.route')
 
 router.get('/', (req, res) => {
     return res.send('Backend for yannn coffee shop')
 })
 router.use('/products', productRoute)
 router.use('/users', usersRoute)
+router.use('/productsDetail', productDetailRoute)
 
 module.exports = router;
